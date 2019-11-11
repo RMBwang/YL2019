d1fee09d8206e49ee270111c01777d5e8997a0e5
BK1917wangwenchao <3142671990@qq.com>
2019-11-11 17:25:44 +0800

    项目初始化

---
+++ src/utils/request.js
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { promises } from "dns"
+
+
+
+const server=axios.create({
+    timeout:3000,
+    // baseUrl:""    
+    withCredentials:true
+})
+
+
+
+//请求的拦截
+
+server.interceptors.request.use((config)=>{
+    if(config.method=="get"){
+        config.params={...config.data}
+    }
+}),(err)=>{
+    Promise.reject(err)
+}
+
+
+//响应的拦截 
+server.interceptors.response.use((res)=>{
+    if(res.status==200){
+        return res.data;
+    }
+})
+
+
+
+//导出
+export default server;
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const path=require("path")
+
+module.exports={
+    devServer:{
+        "/server":{//请求的路径     推荐
+            target:"https://m.228.cn",//跨域要代理的地址
+            changeOrigin:true
+        }
+    } ,
+    configureWebpack:{//值为一个 对象 作用：合并 
+        alias:{
+            "@":path.join(__dirname,"./src"),//用@代替 了 ./src
+            "@assets":path.join(__dirname,"./src/assets"),
+            "@api":path.join(__dirname,"./src/api"),
+            "@common":path.join(__dirname,"./src/common"),
+            "@components":path.join(__dirname,"./src/components"),
+            "@pages":path.join(__dirname,"./src/pages"),
+            "@router":path.join(__dirname,"./src/router"),
+            "@store":path.join(__dirname,"./src/store"),
+            "@utils":path.join(__dirname,"./src/utils"),
+        }
+    }
+}
